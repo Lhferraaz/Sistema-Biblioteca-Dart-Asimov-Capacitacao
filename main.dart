4a70670ef1c40b4817e6188d852328b13e27e264
Lhferraaz <rickzzffx@gmail.com>
2025-12-06 00:18:30 -0300

    Add arquivo main.dart + classe pai Item com atributos e métodos (com exceção da devolução pois surgiram dúvidas)

---
+++ main.dart
@@ -0,0 +1,32 @@
+// Sistema de biblioteca para gerenciamento de livros e revistas - Asimov
+// Linguagem: Dart
+// Autor: Luiz Henrique Ferraz Amaro
+// Data: 03/12/2025
+
+class Item{
+  String titulo;
+  int anoPublicacao;
+  int quantidadeEstoque;
+
+  Item(this.titulo, this.anoPublicacao, this.quantidadeEstoque);
+
+  void emprestar(){
+    assert(quantidadeEstoque > 0, 'Estoque insuficiente para empréstimo.');
+
+    quantidadeEstoque--;
+    print('Empréstimo realizado: $titulo');
+  }
+
+  void devolver(){
+    quantidadeEstoque++;
+    print('Devolução realizada: $titulo');
+  }
+
+  void exibirDetalhes(){
+    String text = '''
+Título: $titulo
+Ano de Publicação: $anoPublicacao
+Quantidade em Estoque: $quantidadeEstoque
+''';
+  }
+}
