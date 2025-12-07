ea75f78858b59f4adfceeecb1cc3eac22055fbdf
Lhferraaz <rickzzffx@gmail.com>
2025-12-07 02:14:32 -0300

    Terminei toda a lógica do código de empréstimo, que deve estar funcionando agora
    + comentários sobre o código que eu fiz até então

--- main.dart
+++ main.dart
@@ -3,36 +3,57 @@
 // Autor: Luiz Henrique Ferraz Amaro
 // Data: 03/12/2025
 
+// Classe de Empréstimo
 class Emprestimo{
-  String idEmp;
-  DateTime dataRetirada;
-  DateTime? dataDevolucao;
+  // Declaração dos atributos
+  int idEmp;
+  DateTime dataRetirada; // Tipo DateTime
+  DateTime? dataDevolucao; // Tipo DateTime
   String status;
 
+  // Construtor + Validações
   Emprestimo({required this.idEmp, required this.dataRetirada, this.status = 'Ativo'}):
+    // Defino a data de devolução para 7 dias após a retirada
     dataDevolucao = dataRetirada.add(Duration(days: 7));
 }
 
-
-
+// Classe Item
 class Item{
+  // Declaração dos atributos da super
   String titulo;
   int anoPublicacao;
   int quantidadeEstoque;
-  List
+  int id = 0;
+  Map <int, Emprestimo> historicoEmprestimos = {}; // Mapa para guardar empréstimos no tipo: id - data
 
+  // Construtor + validações
   Item(this.titulo, this.anoPublicacao, this.quantidadeEstoque):
     assert(quantidadeEstoque >= 0, 'Quantidade em estoque deve ser menor ou igul a 0'),
     assert(titulo.isNotEmpty, 'O Título não pode ser vazio'),
     assert(anoPublicacao > 0 && anoPublicacao < DateTime.now().year, 'Ano de publicação inválido');
-  
-  void emprestar(){
+
+  // Método de empréstimo de livros
+  int emprestar(){
+    // Validação com exception
     if (quantidadeEstoque <= 0){
-      print('Não foi possível realizar o empréstimo. Quantidade em estoque indisponível');
-      return;
+      throw Exception('Empréstimo indisponível. Não há cópias em estoque');
     }
 
-    
+    // Decrementa a quantidade de cópias
+    quantidadeEstoque--;
+
+    // Variáveis para criação do empréstimo
+    DateTime dataRetirada = DateTime.now();
+    int idEmp = id;
+
+    // Crio o novo objeto do tipo Empréstimo
+    var novoEmprestimo = Emprestimo(idEmp: idEmp, dataRetirada: dataRetirada);
+    id++;
+    // Atrelo ao idEmp o próprio novoEmprestimo
+    historicoEmprestimos[idEmp] = novoEmprestimo;
+
+    // Retorno id para devolução
+    return idEmp;
   }
 
   void devolver(){
