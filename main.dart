8d6a6fc03cb04aabf78ec1f2f8f79c1a44188b41
Lhferraaz <rickzzffx@gmail.com>
2025-12-07 02:03:15 -0300

    Add a classe livro com sua validação, ainda a implementar a validação de isbn
    + Add a classe empréstimo para controle, e também comecei a implementar o metodo de empresitmo

--- main.dart
+++ main.dart
@@ -3,32 +3,59 @@
 // Autor: Luiz Henrique Ferraz Amaro
 // Data: 03/12/2025
 
+class Emprestimo{
+  String idEmp;
+  DateTime dataRetirada;
+  DateTime? dataDevolucao;
+  String status;
+
+  Emprestimo({required this.idEmp, required this.dataRetirada, this.status = 'Ativo'}):
+    dataDevolucao = dataRetirada.add(Duration(days: 7));
+}
+
+
+
 class Item{
   String titulo;
   int anoPublicacao;
   int quantidadeEstoque;
+  List
 
-  Item(this.titulo, this.anoPublicacao, this.quantidadeEstoque);
-
+  Item(this.titulo, this.anoPublicacao, this.quantidadeEstoque):
+    assert(quantidadeEstoque >= 0, 'Quantidade em estoque deve ser menor ou igul a 0'),
+    assert(titulo.isNotEmpty, 'O Título não pode ser vazio'),
+    assert(anoPublicacao > 0 && anoPublicacao < DateTime.now().year, 'Ano de publicação inválido');
+  
   void emprestar(){
-    assert(quantidadeEstoque > 0, 'Estoque insuficiente para empréstimo.');
+    if (quantidadeEstoque <= 0){
+      print('Não foi possível realizar o empréstimo. Quantidade em estoque indisponível');
+      return;
+    }
 
-    quantidadeEstoque--;
-    print('Empréstimo realizado: $titulo');
+    
   }
 
   void devolver(){
-    quantidadeEstoque++;
-    print('Devolução realizada: $titulo');
+    // Implementar
   }
 
   String exibirDetalhes(){
     String text = '''
-Título: $titulo
-Ano de Publicação: $anoPublicacao
-Quantidade em Estoque: $quantidadeEstoque
-''';
-
-  return text;
+    Título: $titulo
+    Ano de Publicação: $anoPublicacao
+    Quantidade em Estoque: $quantidadeEstoque
+    ''';
+    return text;
   }
+}
+
+class Livro extends Item{
+  String autor;
+  int isbn;
+
+  Livro(this.autor, this.isbn, super.titulo, super.anoPublicacao, super.quantidadeEstoque):
+    assert(autor.isNotEmpty, 'Autor não pode ser vazio');
+
+  
+
 }
